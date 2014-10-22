57ba8cf422fecd22acc0454a701c5927bd408fb5
goossen <mdgoossen@gmail.com>
2014-10-22 16:13:39 -0400

    email parameters

--- temperature.js
+++ temperature.js
@@ -96,14 +96,21 @@ function _controlTemp(label, tempF) {
    if (label === 'tank') {
       if (tempF > 80.5) {
          console.log('Turning on fan');
-         //_email("Turning on fan", "Turning on fan (" + tempF + "F) at " + new Date());
          scheduler.setPower('button3', 'on');
       } else if (tempF < 80.0) {
          console.log('Turning off fan');
-         //_email("Turning off fan", "Turning off fan (" + tempF + "F) at " + new Date());
          scheduler.setPower('button3', 'off');
       }
    }   
+   if (label === 'heater') {
+      if (tempF > 81.0) {
+         console.log('Turning off heater');
+         scheduler.setPower('button8', 'off');
+      } else if (tempF < 78.0) {
+         console.log('Turning on heater');
+         scheduler.setPower('button8', 'on');
+      }
+   }  
 }
 
 
@@ -169,13 +176,23 @@ function _logTempsToFile() {
 function _emailTemps() {
    var messageBody = 'Date, Room, Tank';
    messageBody += '<br>';
+   var send = false;
 
    for (var i = 0; i < dailyState.length; i++) {
+
+      //if the last csv entry is <81 and >77 skip email
+      var n = dailyState[i].substring(dailyState[i].lastIndexOf(",")+1);
+      if (n<78 || n>80.5) {
+         send = true;
+      }
+
       messageBody += dailyState[i];
       messageBody += '<br>';
    }
 
-   emailer.email(messageBody);
+   if (send) {
+      emailer.email(messageBody);
+   } 
 }
 
 exports.startLogging = function() {
